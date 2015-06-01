3d86bab7b16473f1d197aeb5a11432ee02381ede
Matheus Brasil <matheus.brasil10@gmail.com>
2015-05-31 21:30:55 -0300

    Avoid using packages

---
+++ average/example2.py
@@ -0,0 +1,9 @@
+# Here we're not using any package
+
+# Let's get a array of three numbers, for example
+a = [2, 3, 4]
+# If you want calculate from an user input:
+# a = input("Write here the values that you want calculate the average")
+
+print (sum(a)/len(a))
+# It should return "3"
