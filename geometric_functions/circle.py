1f4812018f78f5b5fd0906e094d5475e148b3943
Matheus Brasil <matheus.brasil10@gmail.com>
2015-06-01 00:11:54 -0300

    Update circle exemple

---
+++ geometric_functions/circle.py
@@ -0,0 +1,15 @@
+# Let's find the area of a circle based on its radius.
+
+# Let's use the "math" lib
+import math
+
+def area(r):
+    pi = math.pi
+    area = ( r ** 2 ) * pi
+    return area
+
+def main():
+    # User should provide the radius of the circle
+    r = int(input('Enter the radius of your circle: '))
+    # We print it :)
+    print "Your circle's area is:", area
