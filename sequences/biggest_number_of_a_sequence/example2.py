28dcdc0170090a7305421bd341740022f37c4955
Matheus Brasil <matheus.brasil10@gmail.com>
2015-06-03 12:41:39 -0300

    Add smaller examples on how to find the biggest number

---
+++ sequences/biggest_number_of_a_sequence/example2.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+# Let's calculate the biggest number of a sequence of three numbers.
+
+# Receive three numbers from array
+numbers [2, 3, 4]
+# Using the inbuilt function max()
+print "The biggest number is", max(numbers)
---
+++ sequences/biggest_number_of_a_sequence/example3.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+
+# Let's calculate the biggest number of a sequence of three numbers.
+
+# Using the inbuilt function max() with multiple arguments
+print "The biggest number is", max(2, 3, 4)
