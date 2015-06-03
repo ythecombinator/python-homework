5a433590e719cef778a351acfcd7bf8d82970f06
Matheus Brasil <matheus.brasil10@gmail.com>
2015-06-03 12:40:45 -0300

    Small fix in comments

--- sequences/fibonacci/example3.py
+++ sequences/fibonacci/example3.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-# Here, we're going to implement the famous Fibonacci sequence, which can be mathematically defined by: Fn = F{n-1} + F{n-2}, onde
+# Here, we're going to implement the famous Fibonacci sequence, which can be mathematically defined by: Fn = F{n-1} + F{n-2}, where
 # F1 = 1 e F2 = 1.
 
 # Implemented using a functional approach, where we can write less and be more performatic :)
