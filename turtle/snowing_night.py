2dab1e6f702f3bff8446c21df3c0f052499d9b7a
Matheus Brasil <matheus.brasil10@gmail.com>
2015-07-04 17:26:29 -0300

    Add a function to draw the tree

--- turtle/snowing_night.py
+++ turtle/snowing_night.py
@@ -37,6 +37,57 @@ class FractalTree():
 
     window.exitonclick()
 
+
+  def drawTree(self, n, length, x, y, prevAngle, sizeOfPen):
+
+    if n == 0:
+      return
+
+    fun.setpos(x, y)
+
+    fun.pensize(sizeOfPen)
+
+    if sizeOfPen == 1:
+      newPenSize = 1
+
+    else:
+      newPenSize = sizeOfPen - 1
+
+    fun.pd()
+
+    angle1 = prevAngle + random.randrange(20, 40)
+    angle2 = prevAngle - random.randrange(20, 40)
+
+    fun.setheading(angle1)
+    fun.forward(length)
+
+    x1 = fun.pos()[0]
+    y1 = fun.pos()[1]
+
+    if n == 1:
+      fun.dot(4, "#27ae60")
+
+    if n == 2:
+      if random.random() <= 0.49:
+        fun.dot(2, "#27ae60")
+        fun.pu()
+        return
+
+    fun.backward(length)
+    fun.setheading(angle2)
+
+    fun.forward(length)
+    x2 = fun.pos()[0]
+    y2 = fun.pos()[1]
+
+    if n == 1:
+      fun.dot(2, "#c0392b")
+
+    fun.pu()
+
+    self.drawTree(n - 1, random.uniform(0.70, 0.95) * length, x1, y1, angle1, newPenSize)
+    self.drawTree(n - 1, random.uniform(0.70, 0.95) * length, x2, y2, angle2, newPenSize)
+
 window = turtle.Screen()
 
 fun = turtle.Turtle()
