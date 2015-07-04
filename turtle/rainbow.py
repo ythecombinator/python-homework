ed5c787316123c6d9c1dba4c83e37fde2c792872
Matheus Brasil <matheus.brasil10@gmail.com>
2015-07-04 01:40:43 -0300

    Give the turtle a name
    Since Mick Jagger is one of the songwriters of the song I intend to
    mention, that's it

--- turtle/rainbow.py
+++ turtle/rainbow.py
@@ -2,14 +2,16 @@ import turtle
 
 import colorsys
 
+jagger = turtle.Turtle()
+
 def drawRainbow():
 
   for i in range(1000):
 
       color = colorsys.hsv_to_rgb(i/1000.0, 1.0, 1.0)
-      turtle.color(color)
-      turtle.forward(i)
-      turtle.right(98)
+      jagger.color(color)
+      jagger.forward(i)
+      jagger.right(98)
 
 drawRainbow()
 
